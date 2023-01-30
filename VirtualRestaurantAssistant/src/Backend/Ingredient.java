bf97bc629b742309402ef10a958d5d107ea3df23
Timothy <lai.timothy77@gmail.com>
2023-01-30 15:05:10 -0500

    added some placeholders

---
+++ VirtualRestaurantAssistant/src/Backend/Ingredient.java
@@ -0,0 +1,5 @@
+package Backend;
+
+public abstract class Ingredient {
+	
+}
---
+++ VirtualRestaurantAssistant/src/Backend/Item.java
@@ -0,0 +1,13 @@
+package Backend;
+public class Item {
+	private String name;
+	private String type;
+	private int quantity;
+	private double price;
+	public Item(String name, String type, int quantity, double price) {
+		this.name = name;
+		this.type = type;
+		this.quantity = quantity;
+		this.price = price
+	}
+}
