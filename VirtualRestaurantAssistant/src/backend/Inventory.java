6e9d39fcdd156966dab8787401bed807e7e18590
Aum Patel <aump@my.yorku.ca>
2023-02-03 17:23:04 -0500

    Update Inventory.java
    worked on Inventory class added some functions to it and implemented a map to store data.

--- VirtualRestaurantAssistant/src/backend/Inventory.java
+++ VirtualRestaurantAssistant/src/backend/Inventory.java
@@ -1,5 +1,44 @@
 package backend;
 
-public class Inventory {
+import java.util.*;
 
+
+public class Inventory {
+	Map<String,List<Ingredient>> inventory ;
+	
+	public Inventory() {
+	inventory =  new HashMap<String,List<Ingredient>>();
+	}
+	
+	//this function returns the list of ingredients
+	/**
+	 * input:- ingredientName 
+	 * output:- List<ingredient>
+	 * 
+	 */
+	public List<Ingredient> getIngredient(String ingredientName){
+		return inventory.get(ingredientName);
+	}
+	
+	/**
+	 * this function checks whether a ingredient is in the inventory.
+	 * input:- ingredientName 
+	 * output:- boolean value accordingly
+	 * 
+	 */
+	
+	public boolean searchIngredient(String ingredientName) {
+		return inventory.containsKey(ingredientName);
+	}
+	
+	/**
+	 * this methods removes ingredients from the map and returns removed value  
+	 * input:- ingredientName 
+	 * output:- List<ingredient>
+	 * 
+	 */
+	public List<Ingredient> removeIngredients(String ingredientName) {
+		return inventory.remove(ingredientName);
+	}
+	
 }
