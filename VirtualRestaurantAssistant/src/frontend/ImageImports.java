298011ae296d5070fbf9c98993861a54765f9cb4
Ali Amin <aliamin745@gmail.com>
2023-02-03 17:11:11 -0500

    Image Resource Class
    Used to access all Images across the UI.

---
+++ VirtualRestaurantAssistant/src/frontend/ImageImports.java
@@ -0,0 +1,30 @@
+package frontend;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class ImageImports {
+	
+	//Home Page Images
+	protected static Image img_customer = new ImageIcon(HomePage.class.getResource("/res/custmrYellow.png")).getImage().getScaledInstance(250, 250, Image.SCALE_SMOOTH);
+	protected static Image frameLogo = new ImageIcon(HomePage.class.getResource("/res/swLogo.png")).getImage();
+	protected static Image imgTitle = new ImageIcon(HomePage.class.getResource("/res/ssvyTitle.png")).getImage();
+	protected static Image imgEating = new ImageIcon(HomePage.class.getResource("/res/eating.png")).getImage().getScaledInstance(250,250, Image.SCALE_SMOOTH);
+
+	//Food Icons for Ordering Page
+	protected static Image imgBread = new ImageIcon(HomePage.class.getResource("/res/bread.png")).getImage();
+	protected static Image imgProt = new ImageIcon(HomePage.class.getResource("/res/protein.png")).getImage();
+	protected static Image imgVegi = new ImageIcon(HomePage.class.getResource("/res/vegi.png")).getImage();
+	protected static Image imgSauces = new ImageIcon(HomePage.class.getResource("/res/sauces.png")).getImage();
+	protected static Image imgCheeses = new ImageIcon(HomePage.class.getResource("/res/cheese.png")).getImage();
+	
+	//Image imports
+	protected static Image img_restr = new ImageIcon(HomePage.class.getResource("/res/restrRed.png")).getImage().getScaledInstance(250, 210, Image.SCALE_SMOOTH);
+	protected static Image img_key = new ImageIcon(HomePage.class.getResource("/res/key.png")).getImage().getScaledInstance(35,35, Image.SCALE_SMOOTH);
+	protected static Image img_user = new ImageIcon(HomePage.class.getResource("/res/person.png")).getImage().getScaledInstance(35,  35,  Image.SCALE_SMOOTH);
+		
+	// MESC
+	protected static Image imgBack = new ImageIcon(HomePage.class.getResource("/res/backIcon.png")).getImage();
+
+}
