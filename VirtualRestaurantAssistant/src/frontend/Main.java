329f840cbe36f396e28aef190e7999bbcb119abd
Ali Amin <aliamin745@gmail.com>
2023-02-06 13:36:18 -0500

    App Main executable.
    Separate class to initialize project, and execute.

---
+++ VirtualRestaurantAssistant/src/frontend/Main.java
@@ -0,0 +1,14 @@
+package frontend;
+
+public class Main {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+
+		HomePage home = new HomePage();
+		home.setVisible(true);
+	}
+
+}
