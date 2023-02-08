bbc5257afc2785edc9618fa653409b9519cec207
Ali Amin <aliamin745@gmail.com>
2023-02-08 15:08:56 -0500

    Added Inventory Init
    Inventory gets initialized

--- VirtualRestaurantAssistant/src/frontend/Main.java
+++ VirtualRestaurantAssistant/src/frontend/Main.java
@@ -1,5 +1,13 @@
 package frontend;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import backend.Beef;
+import backend.Bread;
+import backend.Ingredient;
+import backend.Inventory;
+
 public class Main {
 
 	/**
@@ -7,6 +15,22 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
+		
+		Inventory inventory = Inventory.getInstance();
+		LinkedList<Ingredient> beefs = new LinkedList<>();
+		LinkedList<Ingredient> breads = new LinkedList<>();
+		LinkedList<Ingredient> meatball = new LinkedList<>();
+		
+		for(int i = 0 ; i < 6 ; i++) {
+			beefs.add(new Beef("Beef", 10.0, "Meat"));
+			breads.add(new Bread("Bread", 5.0, "Bread"));
+			meatball.add(new Bread("Meatball", 12.0, "Meatball"));
+		}
+
+		inventory.addIngredient("Bread", breads);
+		inventory.addIngredient("Beef", beefs);
+		inventory.addIngredient("Meatball", meatball);
+		
 		HomePage home = new HomePage();
 		home.setVisible(true);
 		
