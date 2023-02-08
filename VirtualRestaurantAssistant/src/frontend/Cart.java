c6a5951fcee2090b3d9f497f443e73e37ef87cfa
Ali Amin <aliamin745@gmail.com>
2023-02-07 23:39:09 -0500

    Refactored Cart class
    Added comments to Cart and Item class.

--- VirtualRestaurantAssistant/src/frontend/Cart.java
+++ VirtualRestaurantAssistant/src/frontend/Cart.java
@@ -2,14 +2,31 @@ package frontend;
 
 import java.util.LinkedList;
 
+/*
+ * 
+ * Cart class used as a temporary store for current
+ * added items before finally placing Order.
+ * 
+ * */
 public class Cart {
 
 	private LinkedList<Item> itemList;
 	
+	/*
+	 * Cart constructor initializes a LinkedList
+	 * used to represent the Cart.
+	 * */
 	Cart(){
 		itemList = new LinkedList<>();
 	}
 	
+	/*
+	 * Adds the item instance to the Cart, 
+	 * but checks for its presence before adding.
+	 * If item is present then it simply adds to 
+	 * the existing item's quantity with the newly
+	 * added quantity.
+	 * */ 
 	void add(Item item) {
 		
 		if(this.getSize() > 0) {
@@ -25,13 +42,27 @@ public class Cart {
 		}
 	}
 	
+	/*
+	 * Return the Cart LinkedList.
+	 * */
 	LinkedList<Item> getCartContent(){
 		return this.itemList;
 	}
+	
+	// Returns the Size of the Cart.
 	int getSize() {
 		return this.itemList.size();
 	}
 }
+
+/*
+ * Item class to represent the temporary entity
+ * of the items existing in the cart, NOT TO BE 
+ * CONFUSED with the Sandwich object itself. This
+ * is a temporary instance to handle within the 
+ * FrontEnd, to decrease coupling with the backend.
+ * 
+ * */
 class Item {
 	
 	private String name; 
@@ -43,6 +74,7 @@ class Item {
 		this.price = price;
 		this.quantity = quantity;
 	}
+	// Self Explanatory Getter methods
 	String getName() {
 		return this.name;
 	}
@@ -52,7 +84,12 @@ class Item {
 	int getQuantity() {
 		return this.quantity;
 	}
-	void setQuantity(int qty) {
+	
+	/*
+	 * Helper method for the Cart to update existing quantity 
+	 * rather than adding a duplicate item to the receipt.
+	 * */
+	protected void setQuantity(int qty) {
 		this.quantity = qty;
 	}
 	
