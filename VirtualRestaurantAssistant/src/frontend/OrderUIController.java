1ae6bcaffbed85b27e6e38376de847c3bbddddc8
Tim <33475176+TimothyLai77@users.noreply.github.com>
2023-02-09 16:59:08 -0500

    commented OrderUIController

--- VirtualRestaurantAssistant/src/frontend/OrderUIController.java
+++ VirtualRestaurantAssistant/src/frontend/OrderUIController.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 
 public class OrderUIController {
 	
+	/**
+	 * Private method used to get the appropriate Sandwich Creator 
+	 * @param name of sandwich creator needed
+	 * @return a sandwichCreator object 
+	 */
 	private static SandwichCreator setSandwichCreator(String name) {
 		if(name.equals("Chicken")) {
 			return new ChickenSandwichCreator();
@@ -18,14 +23,16 @@ public class OrderUIController {
 	}
 	
 	/**
-	 * 
-	 * @param cartList
-	 * @return
+	 * Goes through the cart and attempts to build the sandwich for each 
+	 * @param carList list of cartItems
+	 * @return List of prices 
 	 */
 	public static List<Double> getSandwichOrder(List<CartItem> cartList) {
 		List<Sandwich> orderBag = new ArrayList<>();
 		for(CartItem item : cartList) {
+			// select a sandwich creator
 			SandwichCreator sandwichCreator = setSandwichCreator(item.getName());
+			// make quantity amount of sandwiches per item
 			for(int i = 0; i < item.getQuantity(); i++) {
 				Sandwich sandwich = sandwichCreator.createSandwich();
 				if(sandwich != null) {
@@ -34,9 +41,8 @@ public class OrderUIController {
 			}
 		}
 		
-		
+		// populate list of prices
 		List<Double> costs = new ArrayList<>();
-		
 		for(Sandwich sandwich : orderBag) {
 			costs.add(sandwich.getCost());
 		}
