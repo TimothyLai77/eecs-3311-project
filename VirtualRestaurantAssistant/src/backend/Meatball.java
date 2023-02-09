47f4e2f8d49ee069436511da841901f0f0e0b02e
Aum Patel <aump@my.yorku.ca>
2023-02-09 02:26:30 -0500

    commented the code in beef.java and meatball.java
    wrote some comments for the code in beef.java and meatball.java

--- VirtualRestaurantAssistant/src/backend/Meatball.java
+++ VirtualRestaurantAssistant/src/backend/Meatball.java
@@ -5,18 +5,35 @@ public class Meatball implements Ingredient {
 	private double price;
 	private String type;
 	
+	/**
+	 * Instantiates meat ball object with the given parameters 
+	 * @param name
+	 * @param price
+	 * @param type
+	 */
 	public Meatball(String name, double price, String type) {
 		this.name = name;
 		this.price = price;
 		this.type = type;
 	}
 	
+	/**
+	 * @return:- this method returns the price of meat ball objects
+	 */
 	public double getPrice() {
 		return this.price;
 	}
+	
+	/**
+	 * @return:- This method returns the name of ingredient
+	 */
 	public String getName() {
 		return this.name;
 	}
+	
+	/**
+	 * @return:- This method returns the type of the ingredient
+	 */
 	public String getType() {
 		return this.type;
 	}
