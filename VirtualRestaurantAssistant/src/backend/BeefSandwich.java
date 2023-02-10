836013f37206456b7159a0ce2b7f1ece13d0363f
dopinde <121323067+dopinde@users.noreply.github.com>
2023-02-10 00:35:36 -0500

    Code comments

--- VirtualRestaurantAssistant/src/backend/BeefSandwich.java
+++ VirtualRestaurantAssistant/src/backend/BeefSandwich.java
@@ -2,8 +2,19 @@ package backend;
 
 import java.util.List;
 
+/**
+ * Class representing a Beef Sandwich.
+ * Extends the abstract class Sandwich.
+ */
+
 public class BeefSandwich extends Sandwich {
 
+
+    /**
+     * Constructs a Beef Sandwich with a list of ingredients.
+     *
+     * @param ingredientList list of ingredients for the sandwich
+     */
 
     public BeefSandwich(List<Ingredient> ingredientList) {
         this.ingredientList = ingredientList;
