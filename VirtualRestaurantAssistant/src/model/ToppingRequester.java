17169a66609236251375b08ae75f0d3483979c44
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2023-03-06 14:15:42 -0500

    requester partially done, waiting on database.

--- VirtualRestaurantAssistant/src/model/ToppingRequester.java
+++ VirtualRestaurantAssistant/src/model/ToppingRequester.java
@@ -1,6 +1,6 @@
 package model;
 import java.util.List;
-
+import java.util.ArrayList;
 public class ToppingRequester {
 	private List<Ingredient> requestedIngredients;
 	private Sandwich baseSandwich;
@@ -9,16 +9,52 @@ public class ToppingRequester {
 		this.baseSandwich = sandwich;
 	}
 	
+	/**
+	 * Method Checks that toppings requested exist in the database. 
+	 * @param requested, list of lists of each ingredient name requested
+	 * @return returns true or false of all the ingredients exist in the inventory
+	 */
 	public boolean requestToppings(List<List<String>> requested) {
 		// check the db and determine if its possible to add the ingredeients
 		// returns t/f if ingredeints exist
 		
-		// parse input string. 
+		// simplify list
+		List<String> simplifiedList = simplifyInputList(requested);
 		
 		
-		return false;
+		//todo: remove when db is implemented.
+		//DBInventory inventory = DBInventory.getInstance();
+
+
+		//make a list of ingredients from the simplifie	d list
+		for(String ingredientName : simplifiedList){
+			// check if ingredient exists
+			if(!inventory.searchIngredient(ingredientName)){
+				return false; // not all ingredients exist
+			}else{
+				Ingredient i = inventory.getIngredient(ingredientName);
+				requestedIngredients.add(i);
+			}
+		}
+		return true; // all ingredients exist in db, can proceed to add toppings
 	}
 	
+	/**
+	 * Private helper method to simplify the input list to a single list of strings
+	 * @param requested
+	 * @return
+	 */
+	private List<String> simplifyInputList(List<List<String>> requested){
+		List<String> simplifiedList = new ArrayList<String>();
+		// simplify data to a single list,
+		for(List<String> sublist : requested){
+			for(String topping : sublist){
+				simplifiedList.add(topping);
+			}
+		}
+		return simplifiedList;
+	}
+
 	/**
 	 * Method that applies the requested toppings to the base sandwich
 	 * @return returns a sandwich with toppings.
@@ -26,13 +62,8 @@ public class ToppingRequester {
 	public Sandwich applyToppings() {
 		// pass in the base sandwich to a constructor that 
 		SandwichWithToppings sandwichWithToppings = new SandwichWithToppings(baseSandwich);
-		//TODO: call the DBINventory here...
 		
 		sandwichWithToppings.addToppings(this.requestedIngredients);
 		return sandwichWithToppings;
 	}
-
-
-	
-	
 }
