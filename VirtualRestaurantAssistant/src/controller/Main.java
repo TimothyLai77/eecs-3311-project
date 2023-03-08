f676d1fb968c7bd815bea076f7d28162c31a3724
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2023-03-07 22:25:33 -0500

    added more ingredients to test

--- VirtualRestaurantAssistant/src/controller/Main.java
+++ VirtualRestaurantAssistant/src/controller/Main.java
@@ -4,12 +4,12 @@ import java.util.LinkedList;
 
 import javax.swing.UIManager;
 
-import ingredients.Beef;
-import ingredients.Bread;
-import ingredients.Chicken;
+
+import ingredients.*;
 import model.Ingredient;
 import model.Inventory;
 import model.MapInventory;
+
 import view.HomePage;
 
 public class Main {
@@ -34,13 +34,19 @@ public class Main {
 		LinkedList<Ingredient> breads = new LinkedList<>();
 		LinkedList<Ingredient> meatball = new LinkedList<>();
 		LinkedList<Ingredient> chicken = new LinkedList<>();
+		LinkedList<Ingredient> tomato = new LinkedList<>();
+		LinkedList<Ingredient> lettuce = new LinkedList<>();
+		LinkedList<Ingredient> cheddar = new LinkedList<>();
 		
 		//Populates our Inventory with the ingredient lists above.
-		for(int i = 0 ; i < 6 ; i++) {
+		for(int i = 0 ; i < 100 ; i++) {
 			beefs.add(new Beef("Beef", 10.0, "Meat"));
 			breads.add(new Bread("Bread", 5.0, "Bread"));
 			meatball.add(new Bread("Meatball", 2.0, "Meatball"));
 			chicken.add(new Chicken("Chicken", 12.0, "Meat"));
+			tomato.add(new Tomato("Tomato", 5, "veggie"));
+			lettuce.add(new Lettuce("Lettuce", 12, "veggie"));
+			cheddar.add(new Cheddar("Cheddar", 3.25, "cheese"));
 		}
 		
 		//Respective lists are pushed to the inventory instance.
@@ -48,11 +54,14 @@ public class Main {
 		inventory.addIngredient("Beef", beefs);
 		inventory.addIngredient("Meatball", meatball);
 		inventory.addIngredient("Chicken", chicken);
+
+		inventory.addIngredient("Tomato", tomato);
+		inventory.addIngredient("Lettuce", lettuce);
+		inventory.addIngredient("Cheddar", cheddar);
 		
 		// Calls the HomePage essentially the APP starting flow.
 		HomePage home = new HomePage();
 		home.setVisible(true);
-		
 	}
 
 }
