ec026b305ff015bd1cbddeea50be02a9b6071e4c
Tim <33475176+TimothyLai77@users.noreply.github.com>
2023-03-08 23:24:46 -0500

    put back in old data for testing mirrors data in the sql script

--- VirtualRestaurantAssistant/src/controller/Main.java
+++ VirtualRestaurantAssistant/src/controller/Main.java
@@ -20,7 +20,8 @@ public class Main {
 	/**
 	 * Launch the application.
 	 */
-	
+
+
 	public static void main(String[] args) {
 
 		// look and feel setting for java swing.
@@ -30,9 +31,56 @@ public class Main {
 		    e.printStackTrace();
 		}
 
+
+		// Gets the current inventory instance --- FROM SINGLETON CLASS
+		// MapInventory inventory = MapInventory.getInstance();
+
+		// // Generates a collection of Ingredient Type
+		// LinkedList<Ingredient> beefs = new LinkedList<>();
+		// LinkedList<Ingredient> breads = new LinkedList<>();
+		// LinkedList<Ingredient> meatball = new LinkedList<>();
+		// LinkedList<Ingredient> chicken = new LinkedList<>();
+		// LinkedList<Ingredient> tomato = new LinkedList<>();
+		// LinkedList<Ingredient> lettuce = new LinkedList<>();
+		// LinkedList<Ingredient> cheddar = new LinkedList<>();
+		// LinkedList<Ingredient> mayo = new LinkedList<>();
+		// LinkedList<Ingredient> ketchup = new LinkedList<>();
+		// LinkedList<Ingredient> american = new LinkedList<>();
+		// LinkedList<Ingredient> veggiePatty = new LinkedList<>();
+
+		// // Populates our Inventory with the ingredient lists above.
+		// for (int i = 0; i < 100; i++) {
+		// 	beefs.add(new Beef("Beef", 6.9, "Meat"));
+		// 	breads.add(new Bread("Bread", 3, "Bread"));
+		// 	meatball.add(new Bread("Meatball", 0.75, "Meatball"));
+		// 	chicken.add(new Chicken("Chicken", 4.25, "Meat"));
+		// 	tomato.add(new Tomato("Tomato", 1.25, "veggie"));
+		// 	lettuce.add(new Lettuce("Lettuce", 3.10, "veggie"));
+		// 	cheddar.add(new Cheddar("Cheddar", 5, "cheese"));
+		// 	mayo.add(new Lettuce("Mayo", 0.75, "sauce"));
+		// 	american.add(new Cheddar("American", 2.50, "cheese"));
+		// 	veggiePatty.add(new Cheddar("Veggiepatty", 5, "vegetable"));
+		// 	ketchup.add(new Cheddar("Ketchup", 0.50, "sauce"));
+		// }
+
+		// // Respective lists are pushed to the inventory instance.
+		// inventory.addIngredient("Bread", breads);
+		// inventory.addIngredient("Beef", beefs);
+		// inventory.addIngredient("Meatball", meatball);
+		// inventory.addIngredient("Chicken", chicken);
+		// inventory.addIngredient("Tomato", tomato);
+		// inventory.addIngredient("Lettuce", lettuce);
+		// inventory.addIngredient("Cheddar", cheddar);
+		// inventory.addIngredient("American", american);
+		// inventory.addIngredient("Mayo", mayo);
+		// inventory.addIngredient("Veggiepatty", veggiePatty);
+		// inventory.addIngredient("Ketchup", ketchup);
+
+
 		// Calls the HomePage essentially the APP starting flow.
 		HomePage home = new HomePage();
 		home.setVisible(true);
 	}
 
 }
+
