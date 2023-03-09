ddbe0f292ec8f2ad04323d5fabc1fc3cd9dd3562
Ali Amin <aliamin745@gmail.com>
2023-03-09 00:27:18 -0500

    Created a Manager Controller class
    Now the SQL operations are through controller, and no longer in the front end.

---
+++ VirtualRestaurantAssistant/src/controller/ManagerUIController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import java.sql.SQLException;
+
+import model.ManagerMain;
+
+public class ManagerUIController {
+	
+	private static ManagerMain mg;
+	
+	public ManagerUIController(){
+		mg = new ManagerMain();
+	}
+	
+	// Add ingredient method
+	public String addIngredient(String name, String type, int qty, double price) throws SQLException {
+		return mg.addIngredient(name, type, qty, price);
+	}
+	// Add existing ingredient method
+	public String addExistingIngredient(String name, int qty) throws SQLException {
+		return mg.addExistingIngredient(name, qty);
+	}
+	// Delete ingredient method
+	public String deleteIngredient(String name) throws SQLException {
+		return mg.deleteEntry(name);
+	}
+	// Update price of ingredient method
+	public String updatePrice(String name, double price) throws SQLException {
+		return mg.updatePrice(name, price);
+	}
+}
